40cc79b7854135a40c4a98c58d39057e07e3d8d0
Jiachun Xiang <jcxiang1230@gmail.com>
2022-06-23 21:57:21 -0400

    Deleted logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,14 +5,13 @@ import "./App.css";
 import Calendar from "./Calendar"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import logo from "./logo.svg";
 
 function App() {
 
   return (
 
     <div className="App">
-      <div><img src={logo} alt="App Logo" />
+      <div><h1>Calendar</h1>
         <Routes>
           <Route path="/Calendar"
             element={
@@ -27,7 +26,7 @@ function App() {
       <Container>
         <Row>
           <Col>
-
+            <Route path="/" element={<Calendar />} />
 
           </Col>
         </Row>
@@ -38,39 +37,57 @@ function App() {
 
 export default App;
 
-
-/*import Signup from "./components/Signup";
-
-import { Container } from "react-bootstrap";
-import { AuthProvider } from "./contexts/UserAuthContext";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-*/
 /*
-          <Route path="/" element={<Calenlar />} />
+import { Routes, Route } from "react-router-dom";
+import { Container, Row, Col, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "./App.css";
+import Header from "./header"
+import HeaderBar from "./headerBar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./contexts/UserAuthContext";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Card from "./components/GroupsPg/Cards";
+import Task from "./components/Tasks/Task";
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+  return (
+
+    <div className="App">
+      <div>{loading ? <img src={logo} alt="App Logo" /> : <UserAuthContextProvider>
+        <Routes>
+          <Route path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>} />
+
+          <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/YourGroups" element={<Card />} />
           <Route path="/YourTasks" element={<Task />} />
-*/
-/*
-function App() {
-  
-  return (
-    
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Router>
-            <AuthProvider>
-              <Routes>
-                <Route path="/signup" element={<Signup />} />
-              </Routes>
-            </AuthProvider>
-          </Router>
-        </div>
+        </Routes>
+
+      </UserAuthContextProvider>}</div>;
+      <Container>
+        <Row>
+          <Col>
+
+
+          </Col>
+        </Row>
       </Container>
-    
+    </div>
   );
 }
 
