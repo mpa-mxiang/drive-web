60ad98fd5b1585d6877cb598b0c08bafc215641a
Michael Lam <michaellam0404@gmail.com>
2022-06-26 09:18:31 -0400

    Add navbar brand

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import {
-  Nav,
-  NavLink,
-  NavMenu,
-  Hamburger
-} from './NavbarElements';
-import { NavDropdown } from 'react-bootstrap';
+import { Nav, NavLink, NavMenu, A } from './NavbarElements';
+import { Container, NavDropdown } from 'react-bootstrap';
 
 const Navbar = () => {
   return (
     <>
       <Nav>
+        <A href="/">
+          Max's Driving School
+        </A>
         <NavMenu>
           <NavLink to="/about" style>
             About
           </NavLink>
-          <NavDropdown title="Packages">
-            <NavDropdown.Item href="groups/group1">Sliver</NavDropdown.Item>
-            <NavDropdown.Item href="groups/group2">Gold</NavDropdown.Item>
-          </NavDropdown>
+          <NavLink to="/packages" style>
+            Packages
+          </NavLink>
           <NavLink to="/calendar" style>
             Book Your Class
           </NavLink>
