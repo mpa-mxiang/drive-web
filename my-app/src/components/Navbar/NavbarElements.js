97165b199b684f3c62656f4d945897860dad1d4e
Michael Lam <michaellam0404@gmail.com>
2022-06-26 01:53:59 -0400

    Modify the navbar

--- my-app/src/components/Navbar/NavbarElements.js
+++ my-app/src/components/Navbar/NavbarElements.js
@@ -1,16 +1,15 @@
-import { FaBars } from 'react-icons/fa';
 import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: #ffc107;
-height: 85px;
+background-color: #fff;
+height: 95px;
+width: 100%;
 display: flex;
-justify-content: space-between;
-padding: 0.2rem calc((100vw - 1000px) / 2);
-z-index: 12;
-/* Third Nav */
-/* justify-content: flex-start; */
+align-items: center;
+padding: 0.5rem 0rem;
+position: relative;
+box-shadow: 0 2px 2px 2px rgba(9, 9, 9, 0.23);
 `;
 
 export const NavLink = styled(Link)`
@@ -18,69 +17,34 @@ color: #808080;
 display: flex;
 align-items: center;
 text-decoration: none;
-padding: 0 1rem;
-height: 100%;
-cursor: pointer;
-&.active {
-	color: #000000;
-}
-`;
-
-export const Bars = styled(FaBars)`
-display: none;
-color: #808080;
+padding: 0;
+margin: 0 3rem;
 @media screen and (max-width: 768px) {
-	display: block;
-	position: absolute;
-	top: 0;
-	right: 0;
-	transform: translate(-100%, 75%);
-	font-size: 1.8rem;
-	cursor: pointer;
+	// centers link text and strips off margin
+	text-align: center;
+	margin: 0;
+  padding: 1rem 0;
 }
 `;
 
 export const NavMenu = styled.div`
+margin-left: auto;
 display: flex;
+padding: 0;
 align-items: center;
-margin-right: -24px;
-/* Second Nav */
-/* margin-right: 24px; */
-/* Third Nav */
-/* width: 100vw;
-white-space: nowrap; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
-`;
-
-export const NavBtn = styled.nav`
-display: flex;
-align-items: center;
-margin-right: 24px;
-/* Third Nav */
-/* justify-content: flex-end;
-width: 100vw; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
-`;
-
-export const NavBtnLink = styled(Link)`
-border-radius: 4px;
-background: #808080;
-padding: 10px 22px;
-color: #000000;
-outline: none;
-border: none;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
 text-decoration: none;
-/* Second Nav */
-margin-left: 24px;
-&:hover {
-	transition: all 0.2s ease-in-out;
-	background: #fff;
-	color: #808080;
+@media screen and (max-width: 768px) {
+	// navigation menu is positioned to start 60px from the top of the document (which is directly below the navbar)
+	position: absolute;
+	top: 60px;
+	left: 0;
+	// stacks the li tags vertically 
+	flex-direction: column;
+ // makes menu span full height and width
+	width: 100%;
+	height: calc(100vh - 77px);
+	background-color: white;
+	border-top: 1px solid black;
 }
 `;
+
--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -2,38 +2,30 @@ import React from 'react';
 import {
   Nav,
   NavLink,
-  Bars,
   NavMenu,
-  NavBtn,
-  NavBtnLink,
+  Hamburger
 } from './NavbarElements';
-import { NavDropdown } from "react-bootstrap";
+import { NavDropdown } from 'react-bootstrap';
 
 const Navbar = () => {
   return (
     <>
       <Nav>
-        <Bars />
-
         <NavMenu>
-          <NavLink to='/about' style>
+          <NavLink to="/about" style>
             About
           </NavLink>
           <NavDropdown title="Packages">
             <NavDropdown.Item href="groups/group1">Sliver</NavDropdown.Item>
             <NavDropdown.Item href="groups/group2">Gold</NavDropdown.Item>
           </NavDropdown>
-          <NavLink to='/calendar' style>
+          <NavLink to="/calendar" style>
             Book Your Class
           </NavLink>
-          <NavLink to='/clients' style>
+          <NavLink to="/clients" style>
             Success Clients
           </NavLink>
-
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
         </NavMenu>
-
       </Nav>
     </>
   );
