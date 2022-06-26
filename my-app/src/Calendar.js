6a2204cf727586482e00e54ca34b1864f2a18a12
Jiachun Xiang <jcxiang1230@gmail.com>
2022-06-25 22:47:36 -0400

    fixed bugs

--- my-app/src/Calendar.js
+++ my-app/src/Calendar.js
@@ -1,43 +1,61 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import CalendarTemplate from 'availability-calendar-react';
+import React from 'react';
 
-function Calendar() {
-    const [availability, setAvailability] = useState([])
-    const Calendar = CalendarTemplate({
-        availability,
-        setAvailability
-    })
+import {
+    AvailabilityCalendar,
+    AvailabilityEvent,
+    MsSinceMidnightRange,
+    Booking,
+    Range,
+    CalendarThemeProp,
+} from 'react-availability-calendar';
+import moment from 'moment';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.scss';
+
+const msInHour = 60 * 60 * 1000;
+
+const App: React.FC = () => {
+    const now = new Date();
+
+    const onAvailabilitySelected = (a: AvailabilityEvent) =>
+        console.log('Availability slot selected: ', a);
+
+    const onChangedCalRange = (r: Range) =>
+        console.log('Calendar range selected (fetch bookings here): ', r);
+
+    const blockOutPeriods: MsSinceMidnightRange[] = [
+        [0 * msInHour, 9 * msInHour],
+        [19 * msInHour, 24 * msInHour],
+    ];
+
+    const bookings: Booking[] = [
+        {
+            startDate: new Date(2020, 2, 1, 19),
+            endDate: new Date(2020, 2, 1, 20),
+        },
+        {
+            startDate: new Date(2020, 2, 1, 16, 30),
+            endDate: new Date(2020, 2, 1, 17),
+        },
+    ];
+
+    const providerTimeZone = 'America/New_York';
+
     return (
-        <div>
-            <Calendar />
+        <div style={{ width: 350 }}>
+            <h1>Calendar</h1>
+            <AvailabilityCalendar
+                bookings={bookings}
+                providerTimeZone={providerTimeZone}
+                moment={moment}
+                initialDate={now}
+                onAvailabilitySelected={onAvailabilitySelected}
+                onCalRangeChange={onChangedCalRange}
+                blockOutPeriods={blockOutPeriods}
+            />
         </div>
     );
-}
+};
 
-=======
-import React, { useState } from "react";
-function Calendar() {
-
-
-    // calendar
-    const [date, setDate] = useState(new Date());
-
-    const onChange = date => {
-        setDate(date);
-    };
-    return (
-
-        <div className="container">
-            <div className="app-wrapper">
-
-                <Calendar onChange={onChange} value={date} />
-                {date.toString()}
-
-
-            </div>
-        </div>
-    );
-}
->>>>>>> parent of 40c52f5... Update Calendar.js
 export default Calendar;
