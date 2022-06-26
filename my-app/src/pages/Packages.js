82a8f1454e52463416a319116e159e359f557a68
Michael Lam <michaellam0404@gmail.com>
2022-06-26 09:00:00 -0400

    Update packages

--- my-app/src/pages/Packages.js
+++ my-app/src/pages/Packages.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { HiCheckCircle, HiXCircle } from 'react-icons/hi';
 
 const Packages = () => {
-	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'Right',
-				alignItems: 'Right',
-				height: '100vh'
-			}}
-		>
-			<h1>Packages</h1>
-		</div>
-	);
+  return (
+    <div>
+      <Table responsive="sm">
+        <thead>
+          <tr>
+            <th>Course Packages</th>
+            <th>Silver</th>
+            <th>Gold</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cost</td>
+            <td>650</td>
+            <td>850</td>
+          </tr>
+          <tr>
+            <td>Hours of in-car training</td>
+            <td>10</td>
+            <td>13</td>
+          </tr>
+          <tr>
+            <td>Hours of in-class teaching</td>
+            <td>20</td>
+            <td>20</td>
+          </tr>
+          <tr>
+            <td>Hours of home study</td>
+            <td>10</td>
+            <td>10</td>
+          </tr>
+          <tr>
+            <td>Use of instructors Vehicle for road test</td>
+            <td><HiXCircle /></td>
+            <td><HiCheckCircle /></td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default Packages;
