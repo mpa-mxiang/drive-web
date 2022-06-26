06d9cbe4561f957deb43c784e0e256667a81fee7
Jiachun Xiang <jcxiang1230@gmail.com>
2022-06-26 00:56:42 -0400

    Changed navbar background color

--- my-app/src/components/Navbar/NavbarElements.js
+++ my-app/src/components/Navbar/NavbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: #63D471;
+background: #ffc107;
 height: 85px;
 display: flex;
 justify-content: space-between;
