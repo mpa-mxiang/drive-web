21d1b848e2deac110e7906a4b9faf81965f124f5
Jiachun Xiang <jcxiang1230@gmail.com>
2022-08-11 23:02:59 -0400

    update calendar

---
+++ my-app/src/pages/datas.js
@@ -0,0 +1,142 @@
+const Datas = {
+    resources: [
+        {
+            id: "spa1",
+            name: "Spa 1"
+        },
+        {
+            id: "spa2",
+            name: "Spa 2"
+        }
+    ],
+    events: [
+        {
+            id: 1,
+            start: "2021-06-21 10:00:00",
+            end: "2021-06-21 12:00:00",
+            resourceId: "spa1",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T100000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 2,
+            start: "2021-06-21 10:00:00",
+            end: "2021-06-21 12:00:00",
+            resourceId: "spa2",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T100000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 3,
+            start: "2021-06-21 12:00:00",
+            end: "2021-06-21 14:00:00",
+            resourceId: "spa1",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T120000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 4,
+            start: "2021-06-21 12:00:00",
+            end: "2021-06-21 14:00:00",
+            resourceId: "spa2",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T120000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 5,
+            start: "2021-06-21 14:00:00",
+            end: "2021-06-21 16:00:00",
+            resourceId: "spa1",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T140000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 6,
+            start: "2021-06-21 14:00:00",
+            end: "2021-06-21 16:00:00",
+            resourceId: "spa2",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T140000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 7,
+            start: "2021-06-21 16:00:00",
+            end: "2021-06-21 18:00:00",
+            resourceId: "spa1",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T160000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 8,
+            start: "2021-06-21 16:00:00",
+            end: "2021-06-21 18:00:00",
+            resourceId: "spa2",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T160000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 9,
+            start: "2021-06-21 18:00:00",
+            end: "2021-06-21 20:00:00",
+            resourceId: "spa1",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T180000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 10,
+            start: "2021-06-21 18:00:00",
+            end: "2021-06-21 20:00:00",
+            resourceId: "spa2",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T180000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 11,
+            start: "2021-06-21 20:00:00",
+            end: "2021-06-21 22:00:00",
+            resourceId: "spa1",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T200000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 12,
+            start: "2021-06-21 20:00:00",
+            end: "2021-06-21 22:00:00",
+            resourceId: "spa2",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T200000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 13,
+            start: "2021-06-21 22:00:00",
+            end: "2021-06-22 00:00:00",
+            resourceId: "spa1",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T220000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        },
+        {
+            id: 14,
+            start: "2021-06-21 22:00:00",
+            end: "2021-06-22 00:00:00",
+            resourceId: "spa2",
+            title: "Prix 120€",
+            rrule: "FREQ=WEEKLY;DTSTART=20210621T220000Z;BYDAY=MO,TU,WE,TH,FR,SA,SU",
+            bgColor: "green"
+        }
+    ]
+};
+
+export default Datas;
