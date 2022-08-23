c783d58a95aa338c8ecb920161b922ebf9ab69b3
Jiachun Xiang <jcxiang1230@gmail.com>
2022-08-22 23:25:27 -0400

    Update Packages.js

--- my-app/src/pages/Packages.js
+++ my-app/src/pages/Packages.js
@@ -7,6 +7,7 @@ const Packages = () => {
   return (
 
     <div>
+      <br></br>
       <Table responsive="sm">
         <thead>
           <tr>
@@ -65,6 +66,84 @@ const Packages = () => {
         </tbody>
 
       </Table>
+      <br></br>
+      <div class="container">
+        <h1>Both prices includes:</h1>
+        <div class="row">
+          <div class="col-md-6 col-xs-12">
+            <p>Each In-Car Session is at least 60 minutes
+              <br></br>
+              One-on-one In-Car Training
+              <br></br>
+              Certified Online through MTO<br></br>
+              <small>(for road test & insurance reduction)</small>
+            </p>
+          </div>
+          <div class="col-md-6 col-xs-12">
+            <p>
+              Free Home, Work or School Pickup for In-Car Lessons<br></br>
+              <small>(for road test & insurance reduction)</small>
+              <br></br>
+              Certificate of Completion<br></br>
+              <small>(sent by email)</small>
+              <br></br>
+              Road Test Booking<br></br>
+              <small>(optional)</small>
+            </p>
+          </div>
+        </div>
+        <br></br>
+        <div class="table-responsive">
+          <table class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th class="text-center">Price</th>
+                <th class="text-center">Package</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <tr>
+                <td>
+                  <h4>$50.00 (HST included)</h4>
+                </td>
+                <td>
+                  <h4>One Hour Freeway Driving Lesson</h4>
+                </td>
+                <td>
+                  Call +1(226)972-6161 to Register
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4>$90.00 (HST included)</h4>
+                </td>
+                <td>
+                  <h4>Two Hours Freeway Driving Lesson</h4>
+                </td>
+                <td>
+                  Call +1(226)972-6161 to Register
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <h4>$190.00 (HST included)</h4>
+                </td>
+                <td>
+                  <h4>G2 Exit Test</h4>
+                  <p>Two hours in-car training plus use of instructor's vehicle for the road test. </p>
+                </td>
+                <td>
+                  Call +1(226)972-6161 to Register
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <br></br>
     </div >
 
 
