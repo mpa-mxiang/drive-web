6b705075825fea429d9692b5d8b1554c72130161
Jiachun Xiang <jcxiang1230@gmail.com>
2022-10-06 20:59:08 -0400

    Update Booking.js

--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import './Booking.css';
 
 function Booking() {
   const [date, setDate] = useState(new Date());
