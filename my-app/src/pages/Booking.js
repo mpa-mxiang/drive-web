06124f6494578fe211707379748fc7d031eb90ab
Jiachun Xiang <jcxiang1230@gmail.com>
2022-10-06 22:31:08 -0400

    Update Booking.js

--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -6,19 +6,43 @@ import "react-datepicker/dist/react-datepicker.css";
 import Time from './Time.js'
 function Booking() {
   const [date, setDate] = useState(new Date());
+  const times = [
+    "08:00 - 10:00",
+    "10:00 - 12:00",
+    "12:00 - 14:00",
+    "14:00 - 16:00",
+    "16:00 - 18:00",
+    "18:00 - 20:00",
+  ];
 
-  return (
-    <div className='app'>
-      <h1 className='text-center'>React Calendar</h1>
-      <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
+  // Loop until we have picked specified number of times
+  while (timesToPick !== timesPicked.length - 1) {
+    // Get a new index and time
+    const index = getRandomNumInRange(0, times.length);
+    const selectedTime = times[index];
+    // If we already picked that time we continue
+    // as we don't want duplicated
+    if (timesPicked.includes(selectedTime)) continue;
+    // Keep the time
+    timesPicked.push(selectedTime);
+  }
 
-      </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
+  // We need to sort the times, as they may not be in a correct order
+  return timesPicked.sort();
+};
+
+return (
+  <div className='app'>
+    <h1 className='text-center'>React Calendar</h1>
+    <div className='calendar-container'>
+      <Calendar onChange={setDate} value={date} />
+
     </div>
-  );
+    <p className='text-center'>
+      <span className='bold'>Selected Date:</span>{' '}
+      {date.toDateString()}
+    </p>
+  </div>
+);
 }
 export default Booking;
