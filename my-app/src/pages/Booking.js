5d86d8b4545e5ade9280c058cd35c7062ddbd6c5
Jiachun Xiang <jcxiang1230@gmail.com>
2022-10-06 21:31:30 -0400

    Update Booking.js

--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -23,71 +23,71 @@ function Booking() {
         />
 
 
-class Booking extends React.Component {
-  constructor(props) {
-    super(props);
-    let now = new Date();
-    let start = moment(
-      new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)
-    );
-    let end = moment(start).add(1, 'days').subtract(1, 'seconds');
-    this.state = {
-      start: start,
-      end: end,
+        class Booking extends React.Component {
+          constructor(props) {
+          super(props);
+        let now = new Date();
+        let start = moment(
+        new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)
+        );
+        let end = moment(start).add(1, 'days').subtract(1, 'seconds');
+        this.state = {
+          start: start,
+        end: end,
     };
 
-    this.applyCallback = this.applyCallback.bind(this);
+        this.applyCallback = this.applyCallback.bind(this);
   }
 
-  applyCallback(startDate, endDate) {
-    this.setState({
-      start: startDate,
-      end: endDate,
-    });
+        applyCallback(startDate, endDate) {
+          this.setState({
+            start: startDate,
+            end: endDate,
+          });
   }
 
-  render() {
-    let now = new Date();
-    let start = moment(
-      new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)
-    );
-    let end = moment(start).add(1, 'days').subtract(1, 'seconds');
-    let ranges = {
-      'Today Only': [moment(start), moment(end)],
-      'Yesterday Only': [
+        render() {
+          let now = new Date();
+        let start = moment(
+        new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0)
+        );
+        let end = moment(start).add(1, 'days').subtract(1, 'seconds');
+        let ranges = {
+          'Today Only': [moment(start), moment(end)],
+        'Yesterday Only': [
         moment(start).subtract(1, 'days'),
         moment(end).subtract(1, 'days'),
-      ],
-      '3 Days': [moment(start).subtract(3, 'days'), moment(end)],
+        ],
+        '3 Days': [moment(start).subtract(3, 'days'), moment(end)],
     };
-    let local = {
-      format: 'DD-MM-YYYY HH:mm',
-      sundayFirst: false,
+        let local = {
+          format: 'DD-MM-YYYY HH:mm',
+        sundayFirst: false,
     };
-    let maxDate = moment(start).add(24, 'hour');
-    return (
-      <div>
-        <DateTimeRangeContainer
-          ranges={ranges}
-          start={this.state.start}
-          end={this.state.end}
-          local={local}
-          maxDate={maxDate}
-          applyCallback={this.applyCallback}
-        >
-          <FormControl
-            id="formControlsTextB"
-            type="text"
-            label="Text"
-            placeholder="Enter text"
-          />
-        </DateTimeRangeContainer>
+        let maxDate = moment(start).add(24, 'hour');
+        return (
+        <div>
+          <DateTimeRangeContainer
+            ranges={ranges}
+            start={this.state.start}
+            end={this.state.end}
+            local={local}
+            maxDate={maxDate}
+            applyCallback={this.applyCallback}
+          >
+            <FormControl
+              id="formControlsTextB"
+              type="text"
+              label="Text"
+              placeholder="Enter text"
+            />
+          </DateTimeRangeContainer>
+        </div>
+        <p className='text-center'>
+          <span className='bold'>Selected Date:</span>{' '}
+          {date.toDateString()}
+        </p>
       </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
-    </div>
-  );
+      );
 }
-export default Booking;
+      export default Booking;
