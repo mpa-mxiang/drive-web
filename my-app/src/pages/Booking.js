570761aae7e539f57b0f7614e89bfbe2c849de46
Jiachun Xiang <jcxiang1230@gmail.com>
2022-10-06 21:59:50 -0400

    add datepicker

--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import './Booking.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function Booking() {
   const [date, setDate] = useState(new Date());
@@ -10,6 +12,12 @@ function Booking() {
       <h1 className='text-center'>React Calendar</h1>
       <div className='calendar-container'>
         <Calendar onChange={setDate} value={date} />
+        <Datepicker
+          controls={['calendar', 'timegrid']}
+          select="range"
+          display="inline"
+          touchUi={true}
+        />
       </div>
       <p className='text-center'>
         <span className='bold'>Selected Date:</span>{' '}
