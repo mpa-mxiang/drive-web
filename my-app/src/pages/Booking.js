e0f7d8419070d612392f63741742a8aa8956f3e9
Jiachun Xiang <jcxiang1230@gmail.com>
2022-10-06 21:16:02 -0400

    update

--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -1,21 +1,67 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import './Booking.css';
+import React from 'react';
+import DateTimeRangeContainer from 'react-advanced-datetimerange-picker'
+import { FormControl } from 'react-bootstrap'
+import moment from "moment"
 
-function Booking() {
-  const [date, setDate] = useState(new Date());
+class Wrapper extends React.Component {
 
-  return (
-    <div className='app'>
-      <h1 className='text-center'>React Calendar</h1>
-      <div className='calendar-container'>
-        <Calendar onChange={setDate} value={date} />
+  constructor(props) {
+    super(props);
+    let now = new Date();
+    let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0));
+    let end = moment(start).add(1, "days").subtract(1, "seconds");
+    this.state = {
+      start: start,
+      end: end
+    }
+
+    this.applyCallback = this.applyCallback.bind(this);
+  }
+
+  applyCallback(startDate, endDate) {
+    this.setState({
+      start: startDate,
+      end: endDate
+    }
+    )
+  }
+
+  render() {
+    let now = new Date();
+    let start = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0));
+    let end = moment(start).add(1, "days").subtract(1, "seconds");
+    let ranges = {
+      "Today Only": [moment(start), moment(end)],
+      "Yesterday Only": [moment(start).subtract(1, "days"), moment(end).subtract(1, "days")],
+      "3 Days": [moment(start).subtract(3, "days"), moment(end)]
+    }
+    let local = {
+      "format": "DD-MM-YYYY HH:mm",
+      "sundayFirst": false
+    }
+    let maxDate = moment(start).add(24, "hour")
+    return (
+      <div>
+        <DateTimeRangeContainer
+          ranges={ranges}
+          start={this.state.start}
+          end={this.state.end}
+          local={local}
+          maxDate={maxDate}
+          applyCallback={this.applyCallback}
+        >
+          <FormControl
+            id="formControlsTextB"
+            type="text"
+            label="Text"
+            placeholder="Enter text"
+          />
+        </DateTimeRangeContainer>
       </div>
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
-    </div>
-  );
+    );
+  }
 }
 export default Booking;
