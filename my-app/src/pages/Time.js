d29c13310071e8b3833837cc3633378a6c9062d0
Jiachun Xiang <jcxiang1230@gmail.com>
2022-10-06 22:18:16 -0400

    Update Time.js

--- my-app/src/pages/Time.js
+++ my-app/src/pages/Time.js
@@ -1,17 +1,35 @@
+import React from 'react'
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import './Booking.css';
-import Times from './Times.js'
 
-import React from 'react'
+const time = ['08:00', '09:00', '10:00', '14:00', '15:00']
 
-function Time(props) {
+function Times(props) {
+
+    const [event, setEvent] = useState(null)
+    const [info, setInfo] = useState(false)
+
+    function displayInfo(e) {
+        setInfo(true);
+        setEvent(e.target.innerText);
+    }
 
     return (
-        <div>
-            {props.showTime ? <Times date={props.date} /> : null}
+
+        <div className="times">
+            {time.map(times => {
+                return (
+                    <div>
+                        <button onClick={(e) => displayInfo(e)}> {times} </button>
+                    </div>
+                )
+            })}
+            <div>
+                {info ? `Your appointment is set to ${event} ${props.date.toDateString()}` : null}
+            </div>
         </div>
     )
 }
 
-export default Time;
+export default Times;
