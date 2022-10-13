090d866c59e6b36d8633c7abeae369a7141af510
Michael Lam <michaellam0404@gmail.com>
2022-10-12 23:47:45 -0400

    Update sms notification

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -2,10 +2,30 @@ import React, { useMemo } from 'react';
 import './About.css';
 import self from '../images/self.jpg';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { Loader, LoaderOptions } from '@googlemaps/js-api-loader';
+
 const About = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   });
+
+  function notify() {
+    fetch('https://textbelt.com/text', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        phone: '6479976478',
+        message: 'test phone message',
+        key: 'textbelt',
+      }),
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      });
+  }
 
   if (!isLoaded) return <div>Loading...</div>;
   return (
@@ -17,6 +37,9 @@ const About = () => {
           Hi, I'm Max, providing Online Digital Driver's Training in Kitchener,
           Waterloo, Cambridge, Palmerston, and Mount Forest for last 12 years.
         </p>
+      </div>
+      <div>
+        <button onClick={notify()}>send message</button>
       </div>
       <div>
         <GoogleMap
