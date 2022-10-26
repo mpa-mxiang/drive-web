a7e06b5b50ff416ef8f46c8a6455c589cbeacf98
Michael Lam <michaellam0404@gmail.com>
2022-10-26 00:52:29 -0400

    Update booking and about

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -3,13 +3,14 @@ import './About.css';
 import self from '../images/self.jpg';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import { Loader, LoaderOptions } from '@googlemaps/js-api-loader';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 const About = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   });
-
-
 
   if (!isLoaded) return <div>Loading...</div>;
   return (
@@ -22,6 +23,24 @@ const About = () => {
           Waterloo, Cambridge, Palmerston, and Mount Forest for last 12 years.
         </p>
       </div>
+
+      <Row xs={1} md={3} className="g-4">
+        {Array.from({ length: 9 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -1,14 +1,11 @@
 import { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './Booking.css';
 import { ScheduleMeeting } from 'react-schedule-meeting';
 import { format } from 'date-fns';
 import Card from 'react-bootstrap/Card';
-import Time from 'react-time';
+
 export default function Booking() {
-  const [date, setDate] = useState(new Date());
-  const [time, setTime] = useState(new Time());
-
-
   const availableTimeslots = [0, 1, 2, 3, 4, 5].map(id => {
     return {
       id,
@@ -22,7 +19,7 @@ export default function Booking() {
       ),
       endTime: new Date(
         new Date(new Date().setDate(new Date().getDate() + id)).setHours(
-          17,
+          22,
           0,
           0,
           0
@@ -31,18 +28,13 @@ export default function Booking() {
     };
   });
 
-  const handleTimeslotClicked = startTimeEventEmit => {
-    const message = `Time selected: ${format(
-      startTimeEventEmit.startTime,
-      'cccc, LLLL do h:mm a'
-    )}`;
-    alert(message);
+  function notify(thisMessage) {
     fetch('https://textbelt.com/text', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         phone: '6475463780',
-        message: message,
+        message: thisMessage,
         key: 'textbelt',
       }),
     })
@@ -52,7 +44,9 @@ export default function Booking() {
       .then(data => {
         console.log(data);
       });
-  };
+  }
+
+  const [date, setDate] = useState('');
 
   return (
     <div className="Booking">
@@ -60,26 +54,22 @@ export default function Booking() {
         borderRadius={10}
         primaryColor="#3f5b85"
         eventDurationInMinutes={120}
-        eventStartTimeSpreadInMinutes={10}
+        eventStartTimeSpreadInMinutes={0}
         availableTimeslots={availableTimeslots}
-        onStartTimeSelect={handleTimeslotClicked}
+        onStartTimeSelect={date => {
+          setDate(
+            `Time selected: ${format(date.startTime, 'cccc, LLLL do h:mm a')}`
+          );
+          //发信息给手机 uncomment下面那条code就行
+          //notify(date);
+        }}
         onNoFutureTimesAvailable={console.log}
-        onSelectedDayChange={date}
-        onStartTimeSelect={time}
-        onChange={[setDate, setTime]}
-        value={[date, time]}
       />
-      <p className='text-center'>
-        <span className='bold'>Selected:</span>
-        <br></br>
-        {date.toDateString()}
-        <br></br>
-
-        <br></br>
-
-      </p>
+      <h2>Selected:</h2>
+      <br></br>
+      <h3>{date}</h3>
+      <br></br>
+      <br></br>
     </div>
-
   );
-
 }
