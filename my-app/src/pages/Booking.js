ef3ade8470155b745b03b86783ca5c8ce75f0a4c
Jiachun Xiang <jcxiang1230@gmail.com>
2022-10-29 23:06:43 -0400

    Update Booking.js

--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -1,141 +1,163 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { useState } from 'react';
 import './Booking.css';
 import { ScheduleMeeting } from 'react-schedule-meeting';
 import { format } from 'date-fns';
-import validator from 'validator'
-export default function Booking() {
-  const availableTimeslots = [0, 1, 2, 3, 4, 5].map(id => {
-    return {
-      id,
-      startTime: new Date(
-        new Date(new Date().setDate(new Date().getDate() + id)).setHours(
-          9,
-          0,
-          0,
-          0
-        )
-      ),
-      endTime: new Date(
-        new Date(new Date().setDate(new Date().getDate() + id)).setHours(
-          22,
-          0,
-          0,
-          0
-        )
-      ),
-    };
-  });
+import validator from 'validator';
 
-
-  const [names, setNames] = useState('');
-  const [number, setNumber] = useState('');
-  const [valid, isValid] = useState('');
-  const [pkg, setPackage] = useState('');
-  const [g1, setG1] = useState('');
-
-  const numberHandleChange = event => {
-
-    isValid(validatePhoneNumber(event.target.value));
+const availableTimeslots = [0, 1, 2, 3, 4, 5].map(id => {
+  return {
+    id,
+    startTime: new Date(
+      new Date(new Date().setDate(new Date().getDate() + id)).setHours(
+        9,
+        0,
+        0,
+        0
+      )
+    ),
+    endTime: new Date(
+      new Date(new Date().setDate(new Date().getDate() + id)).setHours(
+        22,
+        0,
+        0,
+        0
+      )
+    ),
   };
+});
+function validate(name, number, pkg, g1) {
+  const errors = [];
 
-  const validatePhoneNumber = (value) => {
-    let isValidPhoneNumber = validator.isMobilePhone(value, "en-US");
-    console.log(isValidPhoneNumber, value)
-    return isValidPhoneNumber;
-  };
-  const handleChange = e => {
-    const target = e.target;
-    if (target.checked) {
-      setNames(target.value);
-      setNumber(target.value);
-      setPackage(target.value);
-      setG1(target.value);
-    }
-  };
-  const handleSubmit = e => {
-    e.preventDefault();
-  };
-  function notify(thisMessage) {
-    fetch('https://textbelt.com/text', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        phone: '6479976478',
-        message: thisMessage,
-        key: 'textbelt',
-      }),
-    })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      });
+  if (name.length === 0) {
+    errors.push("Name can't be empty");
+  }
+  if (validator.isAlpha(name)) {
+    errors.push("Please enter a valid name");
+  }
+  if (number.length === 0) {
+    errors.push("Number can't be empty");
+  }
+  if (validator.isNumeric(number)) {
+    errors.push("Please enter a valid number");
+  }
+  if (pkg === null) {
+    errors.push("Please choose a package");
+  }
+  if (g1 === null) {
+    errors.push("Please choose if you pass the G1 test");
   }
 
-  const [date, setDate] = useState('');
+  if (g1 === 'No') {
+    errors.push("Please pass G1 first");
+  }
 
-  return (
-    <div className="Booking">
-      <ScheduleMeeting
-        borderRadius={10}
-        primaryColor="#3f5b85"
-        eventDurationInMinutes={120}
-        eventStartTimeSpreadInMinutes={0}
-        availableTimeslots={availableTimeslots}
-        onStartTimeSelect={date => {
-          setDate(
-            `Time selected: ${format(date.startTime, 'cccc, LLLL do h:mm a')}`
-          );
-          //发信息给手机 uncomment下面那条code就行
-          //notify(date);
-        }}
-        onNoFutureTimesAvailable={console.log}
-      />
-      <p>Selected:</p>
-      <p>{date}</p>
-      <div className="center">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input type="text" id="names" name="typedName" onChange={handleChange}
-              value={names} />
-          </label>
-          <label>
-            Phone Number:
-            <input type="text" id="number" name="number" onChange={numberHandleChange, handleChange}
-              value={number} />
-          </label>
-          <p>Choose your package:</p>
-          <div className="center">
-            <label>
-              <input type="radio" value="Bronze" checked={pkg === 'Bronze'} onChange={handleChange} />
-              <span>Bronze</span>
-            </label>
-            <label>
-              <input type="radio" value="Sliver" checked={pkg === 'Sliver'} onChange={handleChange} />
-              <span>Sliver</span>
-            </label>
-          </div>
-          <p>Did you pass G1?</p>
-          <div className="center">
-            <label>
-              <input type="radio" value="Yes" checked={g1 === 'Yes'} onChange={handleChange} />
-              <span>Yes</span>
-            </label>
-            <label>
-              <input type="radio" value="No" checked={pkg === 'No'} onChange={handleChange} />
-              <span>No</span>
-            </label>
-          </div>
+
+  return errors;
+}
+
+export default class SignUpForm extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      number: "",
+      pkg: "",
+      g1: "",
+      date: "",
+
+      errors: []
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const { name, number, pkg, g1 } = this.state;
+
+    const errors = validate(name, number, [pkg, g1]);
+    if (errors.length > 0) {
+      this.setState({ errors });
+      return;
+    }
+
+    // submit the data...
+  }
+
+  render() {
+    const { errors } = this.state;
+    <button onClick={() => this.setState({ count: this.state.count + 1 })}></button>
+    return (
+
+      <div className="Booking" >
+        <ScheduleMeeting
+          borderRadius={10}
+          primaryColor="#3f5b85"
+          eventDurationInMinutes={120}
+          eventStartTimeSpreadInMinutes={0}
+          availableTimeslots={availableTimeslots}
+          /*onStartTimeSelect={() => this.setState({
+            date: this.state.date === `Time selected: ${format(date.startTime, 'cccc, LLLL do h:mm a')}`
+            //发信息给手机 uncomment下面那条code就行
+            //notify(date);
+          }
+          }
+          */
+          onNoFutureTimesAvailable={console.log}
+        />
+        <p>Selected:</p>
+        <p>{date}</p>
+        <form onSubmit={this.handleSubmit}>
+          {errors.map(error => (
+            <p key={error}>Error: {error}</p>
+          ))}
+          <input
+            value={this.state.name}
+            onChange={evt => this.setState({ name: evt.target.value })}
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            value={this.state.number}
+            onChange={evt => this.setState({ number: evt.target.value })}
+            type="text"
+            placeholder="Number"
+          />
+          <input
+            value={this.state.pkg}
+            onChange={evt => this.setState({ pkg: evt.target.value })}
+            type="radio"
+            placeholder="Pkg"
+          />
+          <span>Bronze</span>
+          <input
+            value={this.state.pkg}
+            onChange={evt => this.setState({ pkg: evt.target.value })}
+            type="radio"
+            placeholder="Pkg"
+          />
+          <span>Sliver</span>
+          <input
+            value={this.state.pkg}
+            onChange={evt => this.setState({ pkg: evt.target.value })}
+            type="radio"
+            placeholder="Pkg"
+          />
+          <span>Yes</span>
+          <input
+            value={this.state.pkg}
+            onChange={evt => this.setState({ pkg: evt.target.value })}
+            type="radio"
+            placeholder="Pkg"
+          />
+          <span>No</span>
+          <button type="submit">Submit</button>
         </form>
-
       </div>
-      <p>Your name is {names}, your number is {number}.</p>
-      <div className="center">
-        <button type="submit">Submit</button>
-      </div>
-
-    </div >
-  );
+    );
+  }
 }
