e5367493f4a426f1128a0cb8e20c1f5ca272cf89
Jiachun Xiang <jcxiang1230@gmail.com>
2022-11-04 23:09:50 -0400

    Deleted no use import

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -1,11 +1,6 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import './About.css';
-import self from '../images/self.jpg';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-import { Loader, LoaderOptions } from '@googlemaps/js-api-loader';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import { useLoadScript } from '@react-google-maps/api';
 
 const About = () => {
   const { isLoaded } = useLoadScript({
@@ -17,14 +12,14 @@ const About = () => {
     <div>
       <div className="p-5 text-center">
         <h1 className="mb-5">About Us</h1>
-        
+
         <p className="pt-3">
           Hi, I'm Max, providing Online Digital Driver's Training in Kitchener,
           Waterloo, Cambridge, Palmerston, and Mount Forest for last 12 years.
         </p>
       </div>
 
-  
+
     </div>
   );
 };
