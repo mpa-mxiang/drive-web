e349402dea0f83e7b5a5b6ed50b14b7db42f81b9
Jiachun Xiang <jcxiang1230@gmail.com>
2022-11-05 23:35:32 -0400

    back to prev version

--- my-app/src/pages/Booking.js
+++ my-app/src/pages/Booking.js
@@ -43,10 +43,10 @@ function validate(name, number, pkg, g1) {
   if (!validator.isNumeric(number) && number.length < 10) {
     errors.push('Please enter a valid number');
   }
-  if (pkg.length === 0 || !(pkg === 'Bronze') || !(pkg === 'Sliver')) {
+  if (pkg === undefined || !(pkg === 'Bronze') || !(pkg === 'Sliver')) {
     errors.push('Please choose a package');
   }
-  if (g1.length === 0 || !(g1 === 'Yes') || !(g1 === 'No')) {
+  if (g1 === undefined || !(g1 === 'Yes') || !(g1 === 'No')) {
     errors.push('Please choose if you pass the G1 test');
   }
   if (g1 === 'No') {
@@ -132,7 +132,8 @@ export default class SignUpForm extends React.Component {
           availableTimeslots={availableTimeslots}
           onStartTimeSelect={this.handleDate}
           onNoFutureTimesAvailable={console.log}
-        />        <div className="center">
+        />
+        <div className="center">
           <form onSubmit={this.handleSubmit}>
             <label>
               Name:
@@ -154,16 +155,25 @@ export default class SignUpForm extends React.Component {
             </label>
 
             <p>Choose your package:</p>
-            <RadioGroup
-              aria-labelledby="demo-error-radios"
-              name="pkg"
-              value={this.pkg}
-              onChange={this.handlePkgClick}
-            >
-              <FormControlLabel value="Bronze" control={<Radio />} label="Bronze" />
-              <FormControlLabel value="Sliver" control={<Radio />} label="Sliver" />
-            </RadioGroup>
 
+            <div className="center" onChange={evt => this.setState({ pkg: evt.target.value })}>
+              <br></br>
+              <input
+                value={this.state.pkg}
+                type="radio"
+                placeholder="Pkg"
+                id="Bronze"
+                name="Bronze"
+              />Bronze
+              <input
+                value={this.state.pkg}
+                onClick={evt => this.setState({ pkg: evt.target.value })}
+                type="radio"
+                placeholder="Pkg"
+                id="Sliver"
+                name="Sliver"
+              />Sliver
+            </div>
 
             <p>Did you pass G1?</p>
             <div className="center">
@@ -194,12 +204,10 @@ export default class SignUpForm extends React.Component {
               </button>
 
             </div>
-            {
-              errors.map(error => (
-                <p key={error}>Error: {error}</p>
-              ))
-            }
-          </form >
+            {errors.map(error => (
+              <p key={error}>Error: {error}</p>
+            ))}
+          </form>
         </div >
         <br></br>
       </div >
