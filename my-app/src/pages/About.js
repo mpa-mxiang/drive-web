39cac8612abbdbb4b9fff02af32c635acb5d0cda
Michael Lam <michaellam0404@gmail.com>
2022-11-18 17:39:49 -0500

    update about

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -59,18 +59,6 @@ const About = () => {
               have evolved over the years, sometimes by accident, sometimes on
               purpose (injected humour and the like).
             </p>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a search for 'lorem ipsum' will
-              uncover many web sites still in their infancy. Various versions
-              have evolved over the years, sometimes by accident, sometimes on
-              purpose (injected humour and the like).
-            </p>
           </div>
         </div>
       </div>
@@ -79,18 +67,6 @@ const About = () => {
         <div className="columns content">
           <div className="content-container">
             <h5>Why do we use it?</h5>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a search for 'lorem ipsum' will
-              uncover many web sites still in their infancy. Various versions
-              have evolved over the years, sometimes by accident, sometimes on
-              purpose (injected humour and the like).
-            </p>
             <p>
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout. The
@@ -126,18 +102,6 @@ const About = () => {
         <div className="columns content">
           <div className="content-container">
             <h5>Why do we use it?</h5>
-            <p>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English. Many desktop
-              publishing packages and web page editors now use Lorem Ipsum as
-              their default model text, and a search for 'lorem ipsum' will
-              uncover many web sites still in their infancy. Various versions
-              have evolved over the years, sometimes by accident, sometimes on
-              purpose (injected humour and the like).
-            </p>
             <p>
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout. The
